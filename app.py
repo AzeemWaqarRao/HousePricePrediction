494cb6daab2fc8c43c498ee343992760e6d76a51
AzeemWaqarRao <azeemwaqar2k15@gmail.com>
2022-09-02 20:33:37 +0500

    html done

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
     data = request.json['data']
     data = np.array(list(data.values())).reshape(1,-1)
     data = scaler.transform(data)
@@ -27,6 +27,15 @@ def predict():
     
     return jsonify(result)
 
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    data = np.array(data).reshape(1,-1)
+    data = scaler.transform(data)
+    output = float(model.predict(data)[0])
+
+    return render_template("home.html",prediction_text="Predicted House Price is : "+ str(output))
 
 if __name__ == "__main__":
     app.run()
